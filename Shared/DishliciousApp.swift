e7cbda25a61c1d23d744b4de7f26bc5d0c3f7697
Kejvi Peti <kejvi.peti@zemind.ca>
2021-09-29 16:12:02 -0400

    Initial Commit

---
+++ Shared/DishliciousApp.swift
@@ -0,0 +1,17 @@
+//
+//  DishliciousApp.swift
+//  Shared
+//
+//  Created by Kejvi Peti on 2021-09-29.
+//
+
+import SwiftUI
+
+@main
+struct DishliciousApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
